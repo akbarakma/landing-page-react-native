2922108d84ec9ddad39031be7e2ed8fb69d28882
akbarakma <akbarajo1234@gmail.com>
2020-05-12 18:04:07 +0700

    Proses

--- client/App.js
+++ client/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
 import { Searchbar, Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import walletIcon from './assets/wallet.png';
+import profileIcon from './assets/profileIcon.png';
 import rideIcon from './assets/ride-icon.png';
 import carIcon from './assets/car-icon.png';
 import foodIcon from './assets/foodIcon.png';
 import shopIcon from './assets/shopIcon.png';
+import packageIcon from './assets/packageIcon.png';
+import medIcon from './assets/medIcon.png';
+import massageIcon from './assets/massageIcon.png';
+import more from './assets/more.png';
 
 export default function App() {
   return (
@@ -15,7 +19,9 @@ export default function App() {
         value=''
         style={{ position: 'absolute', top: 40, width: 270, marginLeft: 15 }}
       />
-      <Image source={ walletIcon } style={{ top: 38, right: 15, position: 'absolute' }} />
+      <Image source={ profileIcon } style={{ top: 38, right: 15, position: 'absolute', zIndex: 2 }} />
+
+      <ScrollView>
       <View style={{ alignItems: 'center', marginTop: 100 }}>
         <Card style={{ width: 330 }}>
           <Card.Cover style={{ height: 100 }} source={{ uri: 'https://picsum.photos/700' }} />
@@ -24,24 +30,73 @@ export default function App() {
           </Card.Content>
         </Card>
       </View>
-      <View style={{ display: 'flex', flexDirection: 'row', alignContent: 'center', flexWrap: 'wrap', padding: 20 }}>
-        <View>
-          <Image source={ rideIcon } style={{ marginRight: 15, marginLeft: 15 }} />
+      <View style={{ display: 'flex', flexDirection: 'row', alignContent: 'center', flexWrap: 'wrap', padding: 10 }}>
+        <View style={{ marginBottom: 20 }}>
+          <Image source={ rideIcon } style={{ marginRight: 20, marginLeft: 15 }} />
           <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 20 }}>Ride</Text>
         </View>
-        <View>
-          <Image source={ carIcon } style={{ marginRight: 15, marginLeft: 15 }} />
+        <View style={{ marginBottom: 20 }}>
+          <Image source={ carIcon } style={{ marginRight: 20, marginLeft: 15 }} />
           <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 27 }}>Car</Text>
         </View>
-        <View>
-          <Image source={ foodIcon } style={{ marginRight: 15, marginLeft: 15, height: 50, width: 50 }} />
+        <View style={{ marginBottom: 20 }}>
+          <Image source={ foodIcon } style={{ marginRight: 20, marginLeft: 15, height: 50, width: 50 }} />
           <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 20 }}>Food</Text>
         </View>
-        <View>
-          <Image source={ shopIcon } style={{ marginRight: 15, marginLeft: 15 }} />
+        <View style={{ marginBottom: 20 }}>
+          <Image source={ shopIcon } style={{ marginRight: 20, marginLeft: 15 }} />
           <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 20 }}>Shop</Text>
         </View>
+        <View>
+          <Image source={ packageIcon } style={{ marginRight: 20, marginLeft: 15 }} />
+          <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 20 }}>Send</Text>
+        </View>
+        <View>
+          <Image source={ medIcon } style={{ marginRight: 20, marginLeft: 15 }} />
+          <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 23 }}>Med</Text>
+        </View>
+        <View>
+          <Image source={ massageIcon } style={{ marginRight: 20, marginLeft: 15 }} />
+          <Text style={{ fontSize: 18, marginRight: 10, marginLeft: 5 }}>Massage</Text>
+        </View>
+        <View>
+          <Image source={ more } style={{ marginRight: 20, marginLeft: 11 }} />
+          <Text style={{ fontSize: 18, marginRight: 15, marginLeft: 17 }}>More</Text>
+        </View>
       </View>
+
+      <Text style={{ marginLeft: 15, marginTop: 25, fontSize: 24 }}>Top Pick For You</Text>
+        <ScrollView horizontal={ true } style={{ marginLeft: 10, padding: 10 }}>
+          <Card style={{ width: 280, marginRight: 20 }}>
+            <Card.Cover style={{ height: 120 }} source={{ uri: 'https://picsum.photos/700' }} />
+            <Card.Content>
+              <Title>Lorem Ipsum</Title>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Paragraph>
+            </Card.Content>
+          </Card>
+          <Card style={{ width: 280, marginRight: 20 }}>
+            <Card.Cover style={{ height: 120 }} source={{ uri: 'https://picsum.photos/700' }} />
+            <Card.Content>
+              <Title>Lorem Ipsum</Title>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Paragraph>
+            </Card.Content>
+          </Card>
+          <Card style={{ width: 280, marginRight: 20 }}>
+            <Card.Cover style={{ height: 120 }} source={{ uri: 'https://picsum.photos/700' }} />
+            <Card.Content>
+              <Title>Lorem Ipsum</Title>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Paragraph>
+            </Card.Content>
+          </Card>
+          <Card style={{ width: 280, marginRight: 20 }}>
+            <Card.Cover style={{ height: 120 }} source={{ uri: 'https://picsum.photos/700' }} />
+            <Card.Content>
+              <Title>Lorem Ipsum</Title>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Paragraph>
+            </Card.Content>
+          </Card>
+        </ScrollView>
+      </ScrollView>
     </View>
   );
 }
