3dd7a93e89b7356f2490b44b944b7904658e9a2c
akbarakma <akbarajo1234@gmail.com>
2020-05-12 20:36:23 +0700

    Done

--- client/components/PromoCard.js
+++ client/components/PromoCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Card, Title } from 'react-native-paper';
+import placeholder from '../assets/placeholder.jpg';
 
 const PromoCard = () => {
   return (
     <Card style={{ width: 330, marginBottom: 15, marginLeft: 15 }}>
-      <Card.Cover style={{ height: 150 }} source={{ uri: 'https://picsum.photos/700' }} />
+      <Card.Cover style={{ height: 150 }} source={ placeholder } />
       <Card.Content>
         <Title>Promo Card</Title>
       </Card.Content>
--- client/components/YourPromo.js
+++ client/components/YourPromo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card, Title } from 'react-native-paper';
 import { View } from 'react-native';
+import placeholder from '../assets/placeholder.jpg';
 
 const YourPromo = () => {
   return(
     <View style={{ alignItems: 'center' }}>
       <Card style={{ width: 330 }}>
-        <Card.Cover style={{ height: 100 }} source={{ uri: 'https://picsum.photos/700' }} />
+        <Card.Cover style={{ height: 100 }} source={ placeholder } />
         <Card.Content>
           <Title>Your Promo</Title>
         </Card.Content>
